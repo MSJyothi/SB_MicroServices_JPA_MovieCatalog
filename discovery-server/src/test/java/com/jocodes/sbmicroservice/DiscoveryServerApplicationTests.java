8cc7f03e30444e76ca41c23f055c7d6c637117e8
MSJyothi <jyothi.msoman@gmail.com>
2020-07-17 11:26:29 -0400

    Discovery Server - Eureka

---
+++ discovery-server/src/test/java/com/jocodes/sbmicroservice/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocodes.sbmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
